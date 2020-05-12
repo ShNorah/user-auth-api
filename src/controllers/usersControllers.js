ba769556a08885531bcc6ef21614546fa75fd984
ShNorah <aksharon018@gmail.com>
2020-05-12 10:32:05 +0300

    add signup

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -11,31 +11,60 @@ db.on("error", () => {
   console.log("CONNECTION STATUS: CONNECTION FAILED");
 });
 
-
-//exportind signup, login and view users functions
+//exporting signup, login and view users functions
 module.exports = { //structuring a controller
-  SignUp: (req, res) => {
-        const NewUser = new User({       
-                username: req.body.newuser,
-                password: req.body.password,
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                  });
-                  
-                  NewUser.save();
-                  //promises
-                then((saved)=>{
-                    console.log("NEW USER SAVED:", saved);
-                    return res.status(200).json({message: "Signup successful"});
-                })
-                .catch((error) =>{
-                      console.log("ERROR OCCURED:", error);
-                      return res.status(400).json({message: "Signup failed"});
-                  })
-                  
-                },
-  Login: () => {},
-  ViewUsers: () => {},
+ SignUp: (req, res) =>{
+  const req_username = req.body.username;
+  const req_password = req.body.password;
+  const req_firstName = req.body.firstName;
+  const req_lastName = req.body.lastName;
+
+   //first check if user exists in the database  in the users collection
+   User.findOne({username: req_username})
+
+   .then(user=>{
+     if(user){
+       return res.status(400).json({message: "username already exists"})
+
+     }else{
+       //create a new user
+       const NewUser = new User({
+        username: req_username,
+        password: req_password,
+        firstName: req_firstName,
+        lastName: req_lastName
+      });
+      NewUser.save()
+      .then(saved =>{
+        console.log("New User Saved", saved);
+        return res.status(200).json({message: "Signup successful"})
+      })
+      .catch(error=>{
+       console.log("Error Occured", error);
+       return res.status(400).json({message: "Signup failed"})
+      });
+     }
+   })  
+ }, 
+
+ //login controller
+ Login: (req, res)=>{
+   const login_username = req.body.username;
+   const login_password = req.body.password;
+
+   User.findOne({username: login_username, password: login_password})
+   .then(user=>{
+     if(user){
+       return res.status(200).json({message: "login successful"});
+
+     }else{
+      return res.status(400).json({message: "login failed, no such user exists"})
+     }
+   })
+   .catch(error=>{
+    return res.status(400).json({error: "error occurred"})
+   })
+   
+ },
+ ViewUsers: ()=>{}
 };
-
-
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,8 +1,11 @@
 //import the router and instantiate(get an object of the router)
 
 const router = require('express-promise-router')();  
-const {SignUp} = require('../controllers/usersControllers');
+const {SignUp, Login} = require('../controllers/usersControllers');
 
 router.route("/signup").post(SignUp);
+router.route("/login").post(Login);
 
-module.exports = router;
+module.exports = router; 
+
+//nodemon means node monitor
